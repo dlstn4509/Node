9ccec7a207af9bda40c7318a859060a081357ea7
dlstn4509 <dlstn7609@hanmail.net>
2021-08-27 18:02:25 +0900

    ...

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
+/****************************** global init ********************/
+const port = 3000
 const express = require('express')
-const app  = express()
-app.listen(port, () => { console.log(`http://127.0.0.1:3000`) })
+const app = express()
 
-app.get('/weather', (req, res) => {
-  let {let, lon, appid, units} = req.query
-  if(lat && lon) {
-    // 날씨를 생성하는 프로그램
-    res.status(200).json({
-      name: 'Seoul',
-      main: {temp:36.5},
-      weather: [{icon: '01d', main: '맑음', description: '매우 맑음'}]
-    })
-  }
-  else {
-    res.status(400)
-  }
-})
+
+/***************************** reuter init ********************/
+app.get = ('/', (req, res) => {})
+
+
+/**************************** sever function ******************/
+app.listen(port, () => { console.log('http://127.0.0.1:'+port) })
